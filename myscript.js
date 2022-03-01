d473e26574f569f8b8987fbbb11796674d2482f9
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-01 17:27:43 +0100

    creato un ciclo for per scorrere le img

---
+++ myscript.js
@@ -0,0 +1,56 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+// let thumbnailsWrapper = document.getElementById('thumbnails-img')
+
+
+let carouselContent = '';
+
+for ( i= 0; i > items.length; i++){
+    carouselContent += `
+    <div class="main-img-wrapper d-none">
+        <img id="main-img-element" src="${items[i]}" alt="">
+    </div>`
+};
+
+const mainImage = document.getElementById('main-img');
+
+mainImage.innerHTML += carouselContent;
+
+const mainImageElement = document.getElementsByClassName('main-img-wrapper');
+
+mainImageElement[0].classList.add('d-inline')
+console.log(mainImageElement);
+
+
+
+const downButton = document.getElementById('down-button');
+
+
+// downButton.addEventListener('click', function(){
+    
+//         mainImageWrapper.classList.add("d-inline");
+//         console.log(mainImageWrapper)
+// });
+
